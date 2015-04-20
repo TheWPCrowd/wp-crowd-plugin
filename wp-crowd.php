e95635d9befb0bb9b4265ed3bb2c2174328fe63b
Roy Sivan <roy.x.sivan.-ND@disney.com>
2015-04-20 10:44:00 -0700

    plugin base

---
+++ wp-crowd.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: The WP Crowd
+ * Plugin URI: https://github.com/TheWPCrowd/wp-crowd-plugin
+ * Description: Plugin for CPT and other non-view functionlity
+ * Version: 1.0.0
+ * Author: The WP Crowd
+ * Author URI: http://www.thewpcrowd.com
+ * Text Domain: thewpcrowd
+ * License: GPL3
+ */
+ 
+ ?>
